a95c22f7ceba7ade4fd1d93a99d4cd789678bd7f
zeerox <37295958+SunnyNavani@users.noreply.github.com>
2022-02-08 15:03:25 +0530

    Update and rename TokenPolygon.sol to SunnyToken.sol

---
+++ SunnyToken.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+// OpenZeppelin Contracts v4.4.1 (token/ERC20/ERC20.sol)
+pragma solidity 0.8.1;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/SunnyNavani/Contracts/blob/main/TokenService.sol";
+
+contract SunnyToken is ERC20{
+
+    constructor() ERC20("SunnyNavaniCoin","SNC"){
+        _mint(msg.sender,1000000*(10**uint256(decimals())));
+    }
+
+  
+    function transferAndCall(address _recipient, uint256 _value, bytes _extraData) {
+        transfer(_recipient, _value);
+        require(tokenFallback(msg.sender, _value, _extraData));
+}
+}
